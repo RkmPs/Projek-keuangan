5fe5de6fe8a45ec41fc5d3560ba7244a88678f83
RkmPs <140313382+RkmPs@users.noreply.github.com>
2025-02-03 14:13:20 +0700

    sort by

--- app/Http/Controllers/transaksiController.php
+++ app/Http/Controllers/transaksiController.php
@@ -57,11 +57,63 @@ class transaksiController extends Controller
     }
 
 
-    public function index()
+    public function index(Request $request)
     {
         //
         $transaksi = Transaction::where('user_id', auth()->user()->id)->get();
         $balance = AccountInfo::get('balance');
+
+        //total pemasukan
+        $totalIncome = Transaction::where('user_id', auth()->user()->id)
+            ->where('type', 'income')
+            //untuk pemasukan perminggu
+            ->when($request->has('this_week'), function($query){
+                $query->whereBetween('created_at', [ 
+                    Carbon::now()->startOfWeek(),
+                    Carbon::now()->endOfWeek()]);
+            })
+            //untuk pemasukan perbulan
+            ->when($request->month, function($query, $month){
+                $query->whereMonth('created_at', Carbon::now()->month);
+            })
+            //untuk pemasukan pertahun
+            ->when($request->year, function($query, $year){
+                $query->whereYear('created_at', Carbon:now()->year);
+            })
+            ->sum('amount');
+
+        //total pengeluaran
+        $totalExpense = Transaction::where('user_id', auth()->user()->id)
+            ->where('type', 'expense')
+            //untuk pengeluaran perminggu
+            ->when($request->has('this_week'), function($query){
+                $query->whereBetween('created_at', [ 
+                    Carbon::now()->startOfWeek(),
+                    Carbon::now()->endOfWeek()]);
+            })
+            //untuk pemasukan perbulan
+            ->when($request->month, function($query, $month){
+                $query->whereMonth('created_at', $month);
+            })
+            //untuk pemasukan pertahun
+            ->when($request->year, function($query, $year){
+                $query->whereYear('created_at', $year);
+            })
+            ->sum('amount');
+
+        //transaksi minggu ini
+
+        $weekly  = Transaction::where('user_id', auth()->user()->id)
+            ->whereBetween('created_at', [ 
+                Carbon::now()->startOfWeek(),
+                Carbon::now()->endOfWeek()]);
+
+        //transaksi bulan ini
+
+        $montly = 0;
+
+        //jumlah kategori 
+
 
         return Inertia::render('Dashboard', [
             'transaksi' => $transaksi,

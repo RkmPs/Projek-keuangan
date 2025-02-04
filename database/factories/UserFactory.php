2816ce8c4c0cb8bee5ea6127ec8d52dd834ee7b6
RkmPs <140313382+RkmPs@users.noreply.github.com>
2025-02-04 07:22:16 +0700

    Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -39,6 +40,23 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (User $user) {
+            $user->accountInfo()->create(['balance' => 0]);
+        });
+    }
+
+    public function userAccount(){
+        return $this->state([
+            'name' => 'user',
+            'email' => 'user@user.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('testestes'),
+            'remember_token' => Str::random(10),
         ]);
     }
 }
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Transaction;
+use App\Models\Categories;
+use Faker\Factory;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $users = User::all();
+        $incomeCategories = ['Salary', 'Bonus', 'Freelance'];
+        $expenseCategories = ['Rent', 'Food', 'Transport', 'Utilities', 'Entertainment'];
+
+        foreach ($users as $user) {
+            foreach ($incomeCategories as $category) {
+                Categories::create([
+                    'user_id' => $user->id,
+                    'name' => $category,
+                    'type' => 'Income',
+                ]);
+            }
+
+            foreach ($expenseCategories as $category) {
+                Categories::create([
+                    'user_id' => $user->id,
+                    'name' => $category,
+                    'type' => 'Expense',
+                ]);
+            }
+        }
+    }
+}
